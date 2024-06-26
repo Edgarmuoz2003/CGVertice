9bc9038ca8cb6b92a3d8f8deaea7c41c495de54a
Sneider27 <asneider706@gmail.com>
2024-06-26 11:06:48 -0500

    RESPONSIVO DE NOTICIAS

--- resources/views/sistema/CGV/includes/noticiasBlogCartasSticky.blade.php
+++ resources/views/sistema/CGV/includes/noticiasBlogCartasSticky.blade.php
@@ -1,55 +1,45 @@
 <div class="container">
     <h3 class="text" style="color:#b8860b">Converge</h3>
     <hr>
-    <!-- Div del carrucel con las noticias Destacado -->
+    <!-- Div del carrusel con las noticias Destacado -->
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner"  style="padding-left:7%;">
-            <!-- item activo del carrucel con las noticias Destacado -->
-            <div class="carousel-item active" >
+        <div class="carousel-inner" style="padding-left:7%;">
+            <!-- item activo del carrusel con las noticias Destacado -->
+            <div class="carousel-item active">
                 <div class="row mb-2">
-                    <!-- Primera noticia del carrucel con las noticias Destacado -->
+                    <!-- Primera noticia del carrusel con las noticias Destacado -->
                     <div class="col-md-6">
                         <div class="position-relative">
-                            <div class="col-md-8 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin-left:15%;">
-                                <strong class="d-inline-block mb-2 text-primary-emphasis">Categoria</strong>
-                                <h3 class="mb-0 text" style="color:#b8860b">¿Por qué nos cuesta tomar decisiones?</h3>
+                            <div class="col-md-10 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin: auto;">
+                                <strong class="d-inline-block mb-2 text-primary">Categoria</strong>
+                                <h3 class="mb-0" style="color:#b8860b">¿Por qué nos cuesta tomar decisiones?</h3>
                                 <br>
                                 <p class="card-text mb-auto">
                                     ¡Únete a nuestro reto HOY DECIDO!
-
-                                    En momentos de incertidumbre, tomar decisiones puede ser desafiante. ¿Te gustaría
-                                    descubrir por qué y aprender estrategias para tomar decisiones asertivas?
+                                    En momentos de incertidumbre, tomar decisiones puede ser desafiante. ¿Te gustaría descubrir por qué y aprender estrategias para tomar decisiones asertivas?
                                 </p>
                                 <br>
                                 <!-- Video de la carta -->
-                                <div class="embed-responsive embed-responsive-16by9" >
-                                    <iframe class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/yd4S5qUNuMw?si=OvRZo7eIiFTWhL85"
-                                        allowfullscreen></iframe>
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/yd4S5qUNuMw?si=OvRZo7eIiFTWhL85" allowfullscreen></iframe>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="row g-0 overflow-hidden flex-md-row mb-4 h-md-250 position-relative">
-                            <div class="col-md-8 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin-left:15%;">
-                                <strong class="d-inline-block mb-2 text-primary-emphasis">Categoria</strong>
-                                <h3 class="mb-0 text" style="color:#b8860b">Formación para el trabajo y el desarrollo
-                                    humano</h3>
+                        <div class="position-relative">
+                            <div class="col-md-10 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin: auto;">
+                                <strong class="d-inline-block mb-2 text-primary">Categoria</strong>
+                                <h3 class="mb-0" style="color:#b8860b">Formación para el trabajo y el desarrollo humano</h3>
                                 <br>
                                 <p class="card-text mb-auto">
                                     ¡Descubre las ETDH de la mano de la rectora de COMPUEDU!
-
-                                    ¿Interesado en conocer los fundamentos de las ETDH Estrategias de Transformación
-                                    Digital en las Empresas? Únete a nosotros y explora junto a la rectora de COMPUEDU
-                                    los procesos de prácticas.
+                                    ¿Interesado en conocer los fundamentos de las ETDH Estrategias de Transformación Digital en las Empresas? Únete a nosotros y explora junto a la rectora de COMPUEDU los procesos de prácticas.
                                 </p>
                                 <br>
-                                <!-- Aquí colocamos el video de YouTube -->
+                                <!-- Video de la carta -->
                                 <div class="embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/4s8hv1ccXkQ?si=qoWsebdDwbeaHBIy"
-                                        allowfullscreen></iframe>
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/4s8hv1ccXkQ?si=qoWsebdDwbeaHBIy" allowfullscreen></iframe>
                                 </div>
                             </div>
                         </div>
@@ -57,107 +47,90 @@
                 </div>
             </div>
 
+            <!-- Siguiente item del carrusel con las noticias Destacado -->
             <div class="carousel-item">
                 <div class="row mb-2">
-                    <!-- Primera noticia del carrucel con las noticias Destacado -->
+                    <!-- Primera noticia del carrusel con las noticias Destacado -->
                     <div class="col-md-6">
-                        <div class="row g-0 overflow-hidden flex-md-row mb-4 h-md-250 position-relative">
-                            <div class="col-md-8 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin-left:15%;">
-                                <strong class="d-inline-block mb-2 text-primary-emphasis">Categoria</strong>
-                                <h3 class="mb-0 text" style="color:#b8860b">Darte por Vencido o darle una oportunidad a
-                                    tu empresa?</h3>
+                        <div class="position-relative">
+                            <div class="col-md-10 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin: auto;">
+                                <strong class="d-inline-block mb-2 text-primary">Categoria</strong>
+                                <h3 class="mb-0" style="color:#b8860b">Darte por Vencido o darle una oportunidad a tu empresa?</h3>
                                 <br>
                                 <p class="card-text mb-auto">
-                                    "Descubre con el Abogado William Zapata cómo las leyes de insolvencia pueden salvar
-                                    tus proyectos empresariales. Aprende sobre la insolvencia, su gestión y la caducidad
-                                    de deudas. ¡No te pierdas esta oportunidad de fortalecer tu negocio!"
+                                    "Descubre con el Abogado William Zapata cómo las leyes de insolvencia pueden salvar tus proyectos empresariales. Aprende sobre la insolvencia, su gestión y la caducidad de deudas. ¡No te pierdas esta oportunidad de fortalecer tu negocio!"
                                 </p>
                                 <br>
-                                <div class="embed-responsive embed-responsive-16by9" >
-                                    <iframe class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/mzY32px4SWE?si=lyDhE7kxD3gI7PTL"
-                                        allowfullscreen></iframe>
+                                <!-- Video de la carta -->
+                                <div class="embed-responsive embed-responsive-16by9">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/mzY32px4SWE?si=lyDhE7kxD3gI7PTL" allowfullscreen></iframe>
                                 </div>
-
                             </div>
                         </div>
                     </div>
-
                     <div class="col-md-6">
-                        <div class="row g-0 overflow-hidden flex-md-row mb-4 h-md-250 position-relative">
-                            <div class="col-md-8 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin-left:15%;">
-                                <strong class="d-inline-block mb-2 text-primary-emphasis">Categoria</strong>
-                                <h3 class="mb-0 text" style="color:#b8860b">Mediciones y emisiones ambientales</h3>
+                        <div class="position-relative">
+                            <div class="col-md-10 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin: auto;">
+                                <strong class="d-inline-block mb-2 text-primary">Categoria</strong>
+                                <h3 class="mb-0" style="color:#b8860b">Mediciones y emisiones ambientales</h3>
                                 <br>
                                 <p class="card-text mb-auto">
-                                    ¡¡Descubre las mediciones ambientales con Diagnóstico Ambiental y el Ingeniero
-                                    Alexander Cubaque López! En este video, exploraremos cómo estas mediciones impactan
-                                    en temas como empresas, salud laboral, convivencia y más. No te pierdas esta
-                                    oportunidad de aprender de un experto en el campo. ¡Haz clic para ver el video ahora
-                                    mismo,
+                                    ¡¡Descubre las mediciones ambientales con Diagnóstico Ambiental y el Ingeniero Alexander Cubaque López! En este video, exploraremos cómo estas mediciones impactan en temas como empresas, salud laboral, convivencia y más. No te pierdas esta oportunidad de aprender de un experto en el campo. ¡Haz clic para ver el video ahora mismo!
                                 </p>
                                 <br>
-                                <!-- Aquí colocamos el video de YouTube -->
+                                <!-- Video de la carta -->
                                 <div class="embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/mZIDY30c8Fk?si=l_Cho0cFl2BPN5qR"
-                                        allowfullscreen></iframe>
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/mZIDY30c8Fk?si=l_Cho0cFl2BPN5qR" allowfullscreen></iframe>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <!-- Tercer item del carrusel con las noticias Destacado -->
             <div class="carousel-item">
                 <div class="row mb-2">
-                    <!-- Primera noticia del carrucel con las noticias Destacado -->
+                    <!-- Primera noticia del carrusel con las noticias Destacado -->
                     <div class="col-md-6">
-                        <div class="row g-0 overflow-hidden flex-md-row mb-4 h-md-250 position-relative">
-                            <div class="col-md-8 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin-left:15%;">
-                                <strong class="d-inline-block mb-2 text-primary-emphasis">Categoria</strong>
-                                <h3 class="mb-0 text" style="color:#b8860b">De lo Legal a lo Social. Impactos de la
-                                    Gestión en medio de la contingencia</h3>
+                        <div class="position-relative">
+                            <div class="col-md-10 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin: auto;">
+                                <strong class="d-inline-block mb-2 text-primary">Categoria</strong>
+                                <h3 class="mb-0" style="color:#b8860b">De lo Legal a lo Social. Impactos de la Gestión en medio de la contingencia</h3>
                                 <br>
                                 <p class="card-text mb-auto">
                                     ¡No te pierdas nuestro próximo video!
-                                    Exploraremos cómo las decisiones gubernamentales impactan en la vida de las personas
-                                    y empresas. Únete a la Abogada Andrea Iglesias y al Sociólogo Oscar Chaparro para
-                                    descubrir más.
+                                    Exploraremos cómo las decisiones gubernamentales impactan en la vida de las personas y empresas. Únete a la Abogada Andrea Iglesias y al Sociólogo Oscar Chaparro para descubrir más.
                                 </p>
                                 <br>
+                                <!-- Video de la carta -->
                                 <div class="embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/jGUKMJxUyac?si=0rAV0Ye3CNYJypZk"
-                                        allowfullscreen></iframe>
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/jGUKMJxUyac?si=0rAV0Ye3CNYJypZk" allowfullscreen></iframe>
                                 </div>
                             </div>
-                            <!-- Aquí colocamos el video de YouTube -->
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <div class="row g-0 overflow-hidden flex-md-row mb-4 h-md-250 position-relative">
-                            <div class="col-md-8 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin-left:15%;">
-                                <strong class="d-inline-block mb-2 text-primary-emphasis">Categoria</strong>
-                                <h3 class="mb-0 text" style="color:#b8860b">Hacer empresa si se puede, pero con
-                                    responsabilidad</h3>
+                        <div class="position-relative">
+                            <div class="col-md-10 p-4 d-flex flex-column position-static" style="border: #b8860b 1px solid; margin: auto;">
+                                <strong class="d-inline-block mb-2 text-primary">Categoria</strong>
+                                <h3 class="mb-0" style="color:#b8860b">Hacer empresa si se puede, pero con responsabilidad</h3>
                                 <br>
                                 <p class="card-text mb-auto">
                                     ¡No te pierdas el lanzamiento de Converge TV!
-
-                                    Únete al primer programa diseñado por empresarios para empresarios, donde
-                                    abordaremos tus necesidades de información, actualización y acompañamiento.
+                                    Únete al primer programa diseñado por empresarios para empresarios, donde abordaremos tus necesidades de información, actualización y acompañamiento.
                                 </p>
                                 <br>
+                                <!-- Video de la carta -->
                                 <div class="embed-responsive embed-responsive-16by9">
-                                    <iframe class="embed-responsive-item"
-                                        src="https://www.youtube.com/embed/Rjkiawshj50?si=ZOJLg-Jqqzvsqf3z"
-                                        allowfullscreen></iframe>
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Rjkiawshj50?si=ZOJLg-Jqqzvsqf3z" allowfullscreen></iframe>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 </div>
