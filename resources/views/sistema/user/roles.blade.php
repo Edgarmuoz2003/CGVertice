ec619261b28aa70c2503edf717265cc064d7b3f0
Sebastian Carvajal <juansebastiancarvajalgoez@gmail.com>
2024-06-27 14:57:00 -0500

    Diseño de las opciones del usuario al iniciar sesión

--- resources/views/sistema/user/roles.blade.php
+++ resources/views/sistema/user/roles.blade.php
@@ -14,45 +14,66 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
+    @include('sistema.CGV.includes.head')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
 
 <body>
 
-    <div class="container-fluid">
-        <header class="border-bottom lh-1 py-3">
-            <div class="row flex-lg-nowrap flex-wrap justify-content-center align-items-center">
-
-                <!-- Logo de la empresa -->
-                <div class="col-lg-4 col-md-6 col-6 text-center">
-                    <div class="Converge-div-logo">
-                        <img id="VerIniciobtn" src="/img/Converge.png" alt="Logotipo Converge" href="#" class="img-fluid" />
-                    </div>
+    <div class="botonAdmin">
+        <ul class="navbar-nav">
+            @can('Admin Configuracion')
+                <div class="nav-item dropdown">
+                    <a id="adminDropdown" class="nav-link dropdown-toggle" style="color: #b8860b" href="#"
+                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Admin Configuración
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="adminDropdown">
+                        <li>
+                            <a class="dropdown-item" href="#" id="VerUsersbtn">Usuarios</a>
+                        </li>
+                        <script>
+                            document.getElementById('VerUsersbtn').addEventListener('click', function() {
+                                window.location.href = "{{ route('ruta_Users') }}";
+                            });
+                        </script>
+                        <li>
+                            <a class="dropdown-item" href="#" id="rolesBtn">Roles</a>
+                        </li>
+                        <script>
+                            document.getElementById('rolesBtn').addEventListener('click', function() {
+                                window.location.href = "{{ route('ruta_Roles') }}";
+                            });
+                        </script>
+                        <li>
+                            <a class="dropdown-item" href="#" id="permissionsBtn">Permisos</a>
+                        </li>
+                        <script>
+                            document.getElementById('permissionsBtn').addEventListener('click', function() {
+                                window.location.href = "{{ route('ruta_Perm') }}";
+                            });
+                        </script>
+                    </ul>
                 </div>
-            </div>
+            @endcan
+            &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+        </ul>
+    </div>
+    @include('sistema.CGV.includes.boton')
 
-        </header>
-        <a href="{{ route('inicio') }}" class="btn btn-outline-primary">Regresar</a>
+    <div class="container-fluid">
 
         <main class="row">
+
+
             <div class="col-md-1"></div>
             <div class="col-md-10">
-                @if(session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-                @endif
-                @if(session('error'))
-                <div class="alert alert-danger">
-                    {{ session('error') }}
-                </div>
-                @endif
+                <a href="{{ route('inicio') }}" class="btn btn-outline-warning mt-3 ml-2">
+                    <i class="fas fa-arrow-left"></i>
+                </a>
 
-                <form method="POST" action="{{ route('create-all-roles') }}">
-                    @csrf
-                    <button type="submit" class="btn btn-outline-primary">Crear todos los roles</button>
-                </form>
-                <br>
 
                 <div class="table-responsive">
                     <br>
@@ -72,25 +93,47 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($roles as $role)
-                            <tr class="">
-                                <td> {{$role->id}} </td>
-                                <td> {{$role->name}} </td>
-                                <td>
-                                    <a class="btn btn-outline-success" href="{{ route('roles.edit', $role) }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                                        </svg>
-                                    </a>
-                                </td>
-                            </tr>
+                            @foreach ($roles as $role)
+                                <tr class="">
+                                    <td> {{ $role->id }} </td>
+                                    <td> {{ $role->name }} </td>
+                                    <td>
+                                        <a class="btn btn-outline-success ml-10"
+                                            href="{{ route('roles.edit', $role) }}">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                                                <path
+                                                    d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                                            </svg>
+                                        </a>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
                 </div>
+                <div class="pt-5 pl-5">
+                    @if (session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                    @if (session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('create-all-roles') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-outline-warning">Crear todos los roles</button>
+                    </form>
+                </div>
+
             </div>
-            <div class="col-md-1"></div>
     </div>
 </body>
+
 </html>
