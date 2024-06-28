ea6e294ee8b7409ce2951cbfdc52d5aca83a1f2f
Sebastian Carvajal <juansebastiancarvajalgoez@gmail.com>
2024-06-28 13:06:36 -0500

    Slider

--- resources/views/sistema/CGV/includes/sliderProductos.blade.php
+++ resources/views/sistema/CGV/includes/sliderProductos.blade.php
@@ -3,40 +3,48 @@
         <div class="nav nav-underline me-auto mb-0 mb-lg-0">
             <div class="nav-item">
                 <a class="nav-link" href="V2/">
-                    <h3 class="text-center" style="color: #b8860b;">V2 Suministros</h3>
+                    <h3 class="text-center" style="color: #b8860b;">V2 Suminisssstros</h3>
                 </a>
             </div>
         </div>
     </div>
 
-    <div class="scroll-container">
-        <ul class="autoWidth" class="cs-hidden">
-            @foreach ($V2 as $producto)
-            <li class="item-a">
-                <!-- Product-box -->
-                <div class="product-box">
-                    <!-- heading -->
-                    <strong>{{$producto->nombre}}</strong>
-                    <!-- imagen -->
-                    <img src="{{ asset('imagenesProducto/img/' . $producto->imagen) }}" class="img-fluid rounded" alt="Error con la carga de la imagen para el producto para la camilla de emergencia preciona F5 para solucionarlo" style="width: 250px; height: 250px;">
-                    <div class="buy-price">
-                        <div class="container">
-                            <div class="d-grid gap-2">
-                                <!-- Botón de comprar en WhatsApp -->
-                                <a onclick="window.location.href = 'https://wa.me/573147675259?text=Deseo%20adquirir%20uno%20de%20sus%20productos.'" class="btn btn-success btn-sm">
-                                    Comprar Ahora en WhatsApp
-                                </a>
-                                <button onclick="redirectToProduct('{{ $producto->idProducto }}')" class="btn btn-outline-primary btn-sm">
-                                    Más información
-                                </button>
-                                <!-- Botón de más información -->
+    <div class="sliderP">
+        <div class="slide-track">
+            <ul class="autoWidth">
+                @foreach ($V2 as $producto)
+                    <li class="item-a">
+                        <!-- Product-box -->
+                        <div class="product-box">
+                            <!-- heading -->
+                            <strong>{{ $producto->nombre }}</strong>
+                            <!-- imagen -->
+                            <img src="{{ asset('imagenesProducto/img/' . $producto->imagen) }}"
+                                class="img-fluid rounded"
+                                alt="Error con la carga de la imagen para el producto para la camilla de emergencia presiona F5 para solucionarlo"
+                                style="width: 250px; height: 250px;">
+                            <div class="buy-price">
+                                <div class="container">
+                                    <div class="d-grid gap-2">
+                                        <!-- Botón de comprar en WhatsApp -->
+                                        <a onclick="window.location.href = 'https://wa.me/573147675259?text=Deseo%20adquirir%20uno%20de%20sus%20productos.'"
+                                            class="btn btn-success btn-sm">
+                                            Comprar Ahora en WhatsApp
+                                        </a>
+                                        <button onclick="redirectToProduct('{{ $producto->idProducto }}')"
+                                            class="btn btn-outline-primary btn-sm">
+                                            Más información
+                                        </button>
+                                        <!-- Botón de más información -->
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </li>
-            @endforeach
-        </ul>
+                        
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 </section>
 
@@ -46,7 +54,7 @@
         const url = `${baseUrl}/${idProducto}`;
         window.location.href = url;
     }
-    
+
     function showAlert() {
         Swal.fire({
             title: '¡Alerta!',
@@ -65,7 +73,3 @@
         });
     }
 </script>
-
-</body>
-
-</html>
