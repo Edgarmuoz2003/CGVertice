4723415344348cc52ea05ab0de68e69d04129731
juancarvajalG <juansebastiancarvajalgoez@gmail.com>
2024-08-14 14:51:23 -0500

    ServiciosTeaser completo en html y css

--- resources/views/sistema/CGV/includes/serviciosTeaser.blade.php
+++ resources/views/sistema/CGV/includes/serviciosTeaser.blade.php
@@ -8,11 +8,10 @@
     {{-- CardServiciosTeaser (Buscar asi tal cual en el css) --}}
     <div class="row">
         <div class="col-md-4">
-            <a href="#!" style="text-decoration: none; color: white;">
                 <div class="card">
                     <div class="imgBx">
-                        <img src="img/inicio-destacados-sst.jpg" alt="Educación y Formación.png" style="padding-top: 60px">
-                        <h5 class="text-center" style="color: #b8860b; padding-top:2%">SST (Seguridad y salud en el trabajo)</h5>
+                        <img src="img/inicio-destacados-sst.jpg" alt="Educación y Formación.png">
+                        <h5 class="text-center">Seguridad y salud en el trabajo</h5>
                     </div>
                     <br>
                     <div class="content">
@@ -32,11 +31,10 @@
         </div>
 
         <div class="col-md-4">
-            <a href="#!" style="text-decoration: none; color: white;">
                 <div class="card">
                     <div class="imgBx">
-                        <img src="img/PMIRS.png" alt="Gestion Empresarial.png" style="padding-top: 60px">
-                        <h5 class="text-center" style="color: #b8860b; padding-top:2%">PMIRS</h5>
+                        <img src="img/PMIRS.png" alt="Gestion Empresarial.png">
+                        <h5 class="text-center">PMIRS</h5>
                     </div>
                     <br>
                     <div class="content">
@@ -52,34 +50,30 @@
             </a>
         </div>
         <div class="col-md-4">
-            <a href="#!" style="text-decoration: none; color: white;">
                 <div class="card">
                     <div class="imgBx">
-                        <img src="img/PBR.png" alt="Planificacion de Brigadas de Emergencia"
-                            style="padding-top: 50px">
-                        <h5 class="text-center" style="color: #b8860b; padding-top: 8px">BRP
-                        </h5>
+                        <img src="img/PBR.png" alt="Planificacion de Brigadas de Emergencia">
+                        <h5 class="text-center">BRP</h5>
                     </div>
                     <br>
                     <div class="content">
                         <p>Formamos y capacitamos Brigadas de Respuesta a Emergencias, preparadas para actuar de manera eficiente y efectiva ante cualquier eventualidad. En CG VERTICE, nos aseguramos de que tu equipo esté listo para enfrentar emergencias, garantizando la seguridad de todos en el lugar de trabajo.<br>
                             <br>
                             <a href="/servicios#GestionEdu" class="btn btn-success btn-sm" target="_blank"
-                                rel="noopener noreferrer">
-                                Conozca más
-                            </a>
+                            rel="noopener noreferrer">
+                            Conozca más
+                        </a>
                         </p>
+
                     </div>
                 </div>
             </a>
         </div>
         <div class="col-md-4">
-            <!-- Utiliza las clases de Bootstrap para las columnas, en este caso, se divide en 3 columnas en dispositivos medianos (md) -->
-            <a href="#!" style="text-decoration: none; color: white;">
                 <div class="card">
                     <div class="imgBx">
-                        <img src="img/diseñoPEI.png" alt="IPS Gerencial.png" style="padding-top: 50px">
-                        <h5 class="text-center" style="color: #b8860b; padding-top: 10px">Diseño P.E.I.</h5>
+                        <img src="img/diseñoPEI.png" alt="IPS Gerencial.png">
+                        <h5 class="text-center">Diseño P.E.Idsadas.</h5>
                     </div>
                     <br>
                     <div class="content">
@@ -96,12 +90,11 @@
         </div>
 
         <div class="col-md-4">
-            <a href="#!" style="text-decoration: none; color: white;">
                 <!-- Agrega la URL a la que deseas redirigir cuando se hace clic -->
                 <div class="card">
                     <div class="imgBx">
-                        <img src="img/MallasCurriculares.png" alt="Sistemas de Salud en el Trabajo.png" style="padding-top: 60px">
-                        <h4 class="text-center" style="color: #b8860b; padding-top: 20px">Mallas Curriculares</h4>
+                        <img src="img/MallasCurriculares.png" alt="Sistemas de Salud en el Trabajo.png">
+                        <h5 class="text-center">Mallas Curriculares</h5>
                     </div>
                     <div class="content">
                         <p>Diseñamos mallas curriculares personalizadas para la formación en Seguridad y Salud en el Trabajo. En CG VERTICE, nos enfocamos en crear programas educativos que respondan a las necesidades específicas de tu empresa, promoviendo un aprendizaje efectivo y aplicable.<br>
@@ -116,12 +109,11 @@
             </a>
         </div>
         <div class="col-md-4">
-            <a href="#!" style="text-decoration: none; color: white;">
                 <!-- Agrega la URL a la que deseas redirigir cuando se hace clic -->
                 <div class="card">
                     <div class="imgBx">
-                        <img src="img/img.jpeg" alt="Sistemas de Salud en el Trabajo.png" style="padding-top: 60px">
-                        <h5 class="text-center" style="color: #b8860b; padding-top:2%">Formación a la medida</h5>
+                        <img src="img/img.jpeg" alt="Sistemas de Salud en el Trabajo.png">
+                        <h5 class="text-center">Formación a la medida</h5>
                     </div>
                     <div class="content">
                         <p>La seguridad y salud en el trabajo son aspectos cruciales en cualquier empresa. Con
