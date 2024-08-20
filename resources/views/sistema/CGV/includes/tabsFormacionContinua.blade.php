0da92c25c36503f226476fb2344477012e4eb747
juancarvajalG <juansebastiancarvajalgoez@gmail.com>
2024-08-20 10:13:33 -0500

    Información completa en educación y formación

--- resources/views/sistema/CGV/includes/tabsFormacionContinua.blade.php
+++ resources/views/sistema/CGV/includes/tabsFormacionContinua.blade.php
@@ -2,73 +2,115 @@
     <div id="bodytabsQuienessomos">
         <div class="maintabsQuienessomos">
             <div class="logo-vertice">
-                <h3 style="color: #B8860D">Gestion de calidad</h3>
+                <h3 style="color: #B8860D">Servicios de gestión Instituciones Educativas</h3>
             </div>
             <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active" id="1-tab" data-bs-toggle="tab" data-bs-target="#one-tab-pane"
-                        type="button" role="tab" aria-controls="1-tab-pane" aria-selected="true">Talleres integrales</button>
+                        type="button" role="tab" aria-controls="1-tab-pane" aria-selected="true">Talleres
+                        integrales</button>
                 </li>
                 <li class="nav-item" role="presentation">
                     <button class="nav-link" id="2-tab" data-bs-toggle="tab" data-bs-target="#two-tab-pane" type
-                        of="button" role="tab" aria-controls="2-tab-pane" aria-selected="false">Talleres 2</button>
+                        of="button" role="tab" aria-controls="2-tab-pane" aria-selected="false">Talleres
+                        especializados disruptivos</button>
                 </li>
             </ul>
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="one-tab-pane" role="tabpanel" a ria-labelledby="1-tab"
                     tabindex="0">
-                    <p id="pmaintabsQuienessomos">En Vértice, ofrecemos un catálogo de servicios diseñados para abordar
-                        todas las necesidades de tu empresa, desde Gestión de la Calidad hasta Seguridad y Salud en el
-                        Trabajo. Pero lo que realmente nos diferencia son nuestras ventajas:</p>
+                    <p id="pmaintabsQuienessomos">Los talleres integrales en el ámbito empresarial son herramientas
+                        clave para el desarrollo organizacional. Sirven para diversos propósitos, y sus beneficios
+                        incluyen:</p>
                     <div class="ventajas-vertice">
                         <ul>
-                            <li>Enfoque Personalizado: Trabajamos estrechamente contigo para entender tus necesidades y
-                                ofrecer soluciones a medida.</li>
-                            <li>Equipo Multidisciplinario: Contamos con un equipo de expertos en diversas áreas para
-                                brindarte un servicio integral.</li>
-                            <li>Experiencia Comprobada: Más de una década de experiencia respaldan nuestra calidad y
-                                excelencia.</li>
-                            <li>Reducción de Costos: Nuestras soluciones eficientes pueden ayudarte a ahorrar recursos.
-                            </li>
-                            <li>Compromiso con la Excelencia: Nuestra misión es superar tus expectativas y garantizar tu
-                                satisfacción.</li>
+                            <li><strong style="color: #B8860D">1. Desarrollo de Habilidades:</strong> Facilitan el
+                                desarrollo de habilidades específicas en áreas como liderazgo, comunicación, trabajo en
+                                equipo, resolución de conflictos y gestión del tiempo.</li> <br>
+
+                            <li><strong style="color: #B8860D">2. Cohesión de Equipos:</strong> Fomentan la colaboración
+                                y la cohesión entre los miembros del equipo, mejorando las dinámicas grupales y
+                                fortaleciendo las relaciones laborales.</li><br>
+
+                            <li><strong style="color: #B8860D">3. Resolución de Problemas:</strong> Ayudan a identificar
+                                y abordar problemas dentro de la organización, proporcionando estrategias y soluciones
+                                prácticas para superarlos.</li><br>
+
+                            <li><strong style="color: #B8860D">4. Innovación y Creatividad:</strong> Estimulan la
+                                creatividad y la innovación al ofrecer un espacio para la generación de ideas nuevas y
+                                la resolución de desafíos empresariales de manera creativa.</li><br>
+
+                            <li><strong style="color: #B8860D">5. Mejora de Procesos:</strong> Facilitan la revisión y
+                                mejora de procesos internos, ayudando a optimizar la eficiencia y efectividad en el
+                                funcionamiento de la empresa.</li><br>
+
+                            <li><strong style="color: #B8860D">6. Motivación y Compromiso:</strong> Contribuyen a
+                                aumentar la motivación y el compromiso de los empleados al ofrecer oportunidades de
+                                crecimiento personal y profesional.</li><br>
+
+                            <li><strong style="color: #B8860D">7. Adaptación al Cambio:</strong> Preparan a los
+                                empleados para adaptarse a cambios organizacionales o del mercado, proporcionando
+                                herramientas y técnicas para enfrentar la transición de manera efectiva.</li><br>
+
+                            <li><strong style="color: #B8860D">8. Evaluación y Retroalimentación:</strong> Ofrecen una
+                                plataforma para recibir retroalimentación constructiva y evaluar el desempeño de manera
+                                continua, lo que facilita la mejora continua.</li><br>
+
+                            <li><strong style="color: #B8860D">9. Fortalecimiento de la Cultura Organizacional:</strong>
+                                Refuerzan los valores y la cultura de la empresa, alineando a los empleados con la
+                                misión y visión de la organización.</li><br>
+
+                            En resumen, los talleres integrales son fundamentales para el crecimiento y la adaptación de
+                            las empresas, al mejorar tanto las habilidades individuales como la dinámica grupal y la
+                            eficiencia organizacional.
                         </ul>
                     </div>
                 </div>
                 <div class="tab-pane fade" id="two-tab-pane" role="tabpanel" aria-labelledby="2-tab" tabindex="0">
-                    <p id="pmaintabsQuienessomos">En Vértice, ofrecemos un catálogo de servicios diseñados para abordar
-                        todas las necesidades de tu empresa, desde Gestión de la Calidad hasta Seguridad y Salud en el
-                        Trabajo. Pero lo que realmente nos diferencia son nuestras ventajas:</p>
+                    <p id="pmaintabsQuienessomos">Los talleres personalizados son herramientas diseñadas específicamente
+                        para abordar las necesidades y objetivos particulares de cada empresa. A diferencia de los
+                        talleres estándar, estos talleres adaptan su metodología y contenido según los requerimientos
+                        únicos del cliente, ofreciendo una experiencia más relevante y efectiva.
+
+                        Principales Beneficios de los Talleres Personalizados
+                    </p>
                     <div class="ventajas-vertice">
                         <ul>
-                            <li>Enfoque Personalizado: Trabajamos estrechamente contigo para entender tus necesidades y
-                                ofrecer soluciones a medida.
-                            </li>
-                            <li>Equipo Multidisciplinario: Contamos con un equipo de expertos en diversas áreas para
-                                brindarte un servicio integral.
-                            </li>
-                            <li>Experiencia Comprobada: Más de una década de experiencia respaldan nuestra calidad y
-                                excelencia.
-                            </li>
-                            <li>Reducción de Costos: Nuestras soluciones eficientes pueden ayudarte a ahorrar recursos.
-                            </li>
-                            <li>Compromiso con la Excelencia: Nuestra misión es superar tus expectativas y garantizar tu
-                                satisfacción.
-                            </li>
-                            <li>Enfoque Personalizado: Trabajamos estrechamente contigo para entender tus necesidades y
-                                ofrecer soluciones a medida.
-                            </li>
-                            <li>Equipo Multidisciplinario: Contamos con un equipo de expertos en diversas áreas para
-                                brindarte un servicio integral.
-                            </li>
-                            <li>Experiencia Comprobada: Más de una década de experiencia respaldan nuestra calidad y
-                                excelencia.
-                            </li>
-                            <li>Reducción de Costos: Nuestras soluciones eficientes pueden ayudarte a ahorrar recursos.
-                            </li>
-                            <li>Compromiso con la Excelencia: Nuestra misión es superar tus expectativas y garantizar tu
-                                satisfacción.
-                            </li>
+                            <li> <strong style="color: #B8860D">Alineación con Necesidades Específicas:</strong> Cada taller se adapta a las necesidades y
+                                desafíos particulares de la empresa, garantizando que el contenido y las actividades
+                                sean directamente aplicables a su contexto y objetivos.</li> <br>
+
+                            <li><strong style="color: #B8860D">Desarrollo de Habilidades Relevantes:</strong> Se enfocan en desarrollar habilidades que son
+                                críticas para el éxito de la organización, como liderazgo, comunicación, gestión de
+                                proyectos o innovación, basándose en las áreas que requieren mayor atención.</li><br>
+
+                            <li><strong style="color: #B8860D">Cohesión y Dinámicas de Equipo:</strong> Ofrecen actividades y estrategias diseñadas para mejorar
+                                la dinámica del equipo y fortalecer las relaciones laborales, ajustándose a la
+                                estructura y cultura específica de la empresa.</li><br>
+
+                            <li><strong style="color: #B8860D">Solución de Problemas Concretos:</strong> Permiten abordar problemas específicos identificados
+                                por la empresa, proporcionando soluciones y estrategias prácticas que se pueden
+                                implementar de inmediato.</li><br>
+
+                            <li><strong style="color: #B8860D">Fomento de la Innovación:</strong> Estimulan la creatividad y la generación de ideas dentro del
+                                contexto particular de la empresa, facilitando la búsqueda de soluciones innovadoras
+                                adaptadas a sus desafíos y oportunidades.</li><br>
+
+                            <li><strong style="color: #B8860D">Optimización de Procesos:</strong> Revisan y mejoran procesos internos de manera personalizada,
+                                adaptando las recomendaciones a las particularidades de la empresa para aumentar la
+                                eficiencia y efectividad.</li><br>
+
+                            <li> <strong style="color: #B8860D">Incremento de la Motivación:</strong> Al estar alineados con las metas y valores de la empresa,
+                                estos talleres refuerzan el compromiso y la motivación de los empleados al demostrar que
+                                se valoran sus necesidades y se invierte en su desarrollo.</li><br>
+
+                            <li> <strong style="color: #B8860D">Adaptación al Cambio:</strong> Preparan al personal para enfrentar cambios organizacionales o
+                                del mercado de manera específica, proporcionando herramientas adaptadas a la realidad de
+                                la empresa.</li><br>
+
+                            <li><strong style="color: #B8860D">Evaluación y Retroalimentación Personalizada:</strong> Ofrecen un espacio para recibir
+                                retroalimentación específica y evaluar el progreso según las metas establecidas por la
+                                empresa, facilitando una mejora continua y adaptada.</li>
                         </ul>
                     </div>
                 </div>
