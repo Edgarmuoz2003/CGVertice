40db6dfb233da1273520e8daf661de37d96d04ef
Edgar Munoz <edgarmunoz2003@gmail.com>
2024-08-23 08:47:15 -0500

    pqrs

--- resources/views/sistema/CGV/includes/footer.blade.php
+++ resources/views/sistema/CGV/includes/footer.blade.php
@@ -135,6 +135,9 @@
         </div>
         <div class="py-2">
             <div class="container text-center">
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSepqsy8LoKLZ46zXR29qGY2mDUH9HL9L7fmahTKbYFIFoT4GQ/viewform?usp=pp_url" class="text-primary fw-bold" target="_blank">
+                    <i class="bi bi-envelope-fill"></i> PQRS
+                </a>
                 <p class="text-gold mb-0 py-2" style="color:rgb(121, 121, 121)">&copy; 2024 Corporate Group Vértice
                 </p>
             </div>
