9bf9cc79abb96939b32e954b8d96256184d06bb7
juancarvajalG <juansebastiancarvajalgoez@gmail.com>
2024-08-29 09:36:55 -0500

    Botones de whatsapp y style

--- resources/views/sistema/CGV/includes/botonWhatsapp.blade.php
+++ resources/views/sistema/CGV/includes/botonWhatsapp.blade.php
@@ -1,6 +1,11 @@
-<section>
-    <div class="container text-center mt-3">
-        <a href="https://wa.me/573016280574?text=Quiero%20más%20información%20." target="_blank">
+<section class="botoneswp">
+    <div>
+        <p class="btn-wsp2">Pública</p>
+        <a href="https://wa.me/573054627276?text=Quiero%20más%20información." target="_blank">
+            <img src={{ asset('../img/whatsl.jpeg') }} alt="logo watsap" class="btn-wsp2">
+        </a>
+        <p>Linea de atención clientes vértice</p>
+        <a href="https://wa.me/573016280574?text=Quiero%20más%20información." target="_blank">
             <img src={{ asset('../img/whatsl.jpeg') }} alt="logo watsap" class="btn-wsp">
         </a>
     </div>
