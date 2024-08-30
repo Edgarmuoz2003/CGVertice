452a6632ab93cda956966723c0373faadb7d3ebc
juancarvajalG <juansebastiancarvajalgoez@gmail.com>
2024-08-30 08:27:02 -0500

    Botones modificados

--- resources/views/sistema/CGV/includes/botonWhatsapp.blade.php
+++ resources/views/sistema/CGV/includes/botonWhatsapp.blade.php
@@ -1,10 +1,6 @@
-<section class="botoneswp">
-    <div>
-        <a href="https://wa.me/573054627276?text=Quiero%20más%20información." target="_blank">
-            <img src={{ asset('../img/whatsl.jpeg') }} alt="logo watsap" class="btn-wsp2">
-        </a>
-        <a href="https://wa.me/573016280574?text=Quiero%20más%20información." target="_blank">
-            <img src={{ asset('../img/whatsl.jpeg') }} alt="logo watsap" class="btn-wsp">
-        </a>
-    </div>
-</section>
+<a href="https://wa.me/573054627276?text=Quiero%20más%20información." target="_blank">
+    <img src={{ asset('../img/whatsl.jpeg') }} alt="logo watsap" class="btn-wsp2">
+</a>
+<a href="https://wa.me/573016280574?text=Quiero%20más%20información." target="_blank">
+    <img src={{ asset('../img/whatsl.jpeg') }} alt="logo watsap" class="btn-wsp">
+</a>
