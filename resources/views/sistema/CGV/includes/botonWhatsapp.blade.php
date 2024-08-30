c4609060ddf73c2c0930b0891ddfbf5d1a91fc5a
juancarvajalG <juansebastiancarvajalgoez@gmail.com>
2024-08-30 10:59:48 -0500

    Cambios en los estilos de botones

--- resources/views/sistema/CGV/includes/botonWhatsapp.blade.php
+++ resources/views/sistema/CGV/includes/botonWhatsapp.blade.php
@@ -1,7 +1,7 @@
-<a href="https://wa.me/573054627276?text=Quiero%20más%20información." target="_blank" class="btn-wsp" data-text="Ventas">
-    <img src="{{ asset('../img/whatsl.jpeg') }}" alt="logo watsap" style="width: 100%; height: 100%; border-radius: 50%;">
+<a href="https://wa.me/573054627276?text=Quiero%20más%20información." target="_blank" class="btn-wsp" data-text="Servicio a clientes Vértice">
+    <img src="{{ asset('../img/WhatsApp.png') }}" alt="logo watsap" style="width: auto; height: 70%;">
 </a>
 
-<a href="https://wa.me/573016280574?text=Quiero%20más%20información." target="_blank" class="btn-wsp2" data-text="Servicio al Cliente">
-    <img src="{{ asset('../img/whatsl.jpeg') }}" alt="logo watsap" style="width: 100%; height: 100%; border-radius: 50%;">
+<a href="https://wa.me/573016280574?text=Quiero%20más%20información." target="_blank" class="btn-wsp2" data-text="Linea de atención general">
+    <img src="{{ asset('../img/WhatsApp.png') }}" alt="logo watsap" style="width: auto; height: 70%;">
 </a>
