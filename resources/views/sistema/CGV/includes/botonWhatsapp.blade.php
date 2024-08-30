82be333a2440f779110ecbe4d24776ef22b1891a
juancarvajalG <juansebastiancarvajalgoez@gmail.com>
2024-08-30 09:48:56 -0500

    ..

--- resources/views/sistema/CGV/includes/botonWhatsapp.blade.php
+++ resources/views/sistema/CGV/includes/botonWhatsapp.blade.php
@@ -1,6 +1,5 @@
 <a href="https://wa.me/573054627276?text=Quiero%20más%20información." target="_blank">
     <img src={{ asset('../img/whatsl.jpeg') }} alt="logo watsap" class="btn-wsp2">
-    <span class="tooltip">Botón para clientes</span>
 </a>
 <a href="https://wa.me/573016280574?text=Quiero%20más%20información." target="_blank">
     <img src={{ asset('../img/whatsl.jpeg') }} alt="logo watsap" class="btn-wsp">
